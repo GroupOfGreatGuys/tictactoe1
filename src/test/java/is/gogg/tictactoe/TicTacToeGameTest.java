1c2fc71e470a677ba43a29495c311bc3d8492164
arnorymir <arnorymir@gmail.com>
2013-11-10 16:04:42 +0000

    added test for chekForWin method

--- src/test/java/is/gogg/tictactoe/TicTacToeGameTest.java
+++ src/test/java/is/gogg/tictactoe/TicTacToeGameTest.java
@@ -7,10 +7,24 @@ import org.junit.Test;
 
 
 public class TicTacToeGameTest {
+	@Test
+	public void checkConstructor() {
+		TicTacToeGame game = new TicTacToeGame();
+		assertFalse(game.isGameOver());
+   	 }
+
     @Test
-    public void checkConstructor() {
-         TicTacToeGame game = new TicTacToeGame();
-        assertFalse(game.isGameOver());
+    public void checkForWinTest()
+    {
+    
+    	TicTacToeGame game = new TicTacToeGame();
+   		
+   		game.makeTurn('x',0);
+		game.makeTurn('x',1);
+   		game.makeTurn('x',2);
+
+    	assertTrue(game.isGameOver());
+
     }
 
 }
