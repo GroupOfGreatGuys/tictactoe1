d050acecc49c9bb2a5dcc0d15b1d5a7f2098d616
matthiassig12 <matsig91@gmail.com>
2013-11-10 14:19:21 +0000

    added functions to tictactoegame

--- src/main/java/is/gogg/tictactoe/TicTacToeGame.java
+++ src/main/java/is/gogg/tictactoe/TicTacToeGame.java
@@ -10,6 +10,19 @@ public class TicTacToeGame
 		gameOver = false;
 		squares = new char[9];
 	}
+	
+	public void makeTurn(char sign, int index)
+	{
+		// Makes a player turn, puts the player's sign in the correct index
+		squares[index] = sign;
+		checkForWin();
+	}
+	
+	public void checkForWin()
+	{
+		// Checks if the game is over
+	}
+
 	public void draw()
 	{
 		// This function draws up the board
