5a19156fe9df39170d678f28e47b0cc84568029a
matthiassig12 <matsig91@gmail.com>
2013-11-13 20:46:02 +0000

    Added player class as well as test file for player

---
+++ src/main/java/is/gogg/tictactoe/Player.java
@@ -0,0 +1,12 @@
+package is.gogg.tictactoe;
+
+public class Player
+{
+	private String name;
+	private boolean isPlaying;
+	
+	public Player(String theName)
+	{
+		
+	}
+}
---
+++ src/test/java/is/gogg/tictactoe/PlayerTest.java
@@ -0,0 +1,14 @@
+package is.gogg.tictactoe;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class PlayerTest {
+	@Test
+	public void testConstructor() {
+		assertEquals(1,1);
+   	 }
+}
