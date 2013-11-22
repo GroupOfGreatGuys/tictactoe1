1610d48bdab6b75e1c120bf0823857913260c7f9
matthiassig12 <matsig91@gmail.com>
2013-11-22 14:46:10 +0000

    Tictactoe is playable(barely), but not testable(?)

--- src/main/java/is/gogg/tictactoe/TicTacToe.java
+++ src/main/java/is/gogg/tictactoe/TicTacToe.java
@@ -1,11 +1,85 @@
 package is.gogg.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToe
 {
+		private Board board;
+		private Player p1;
+		private Player p2;
+		
+		public TicTacToe(Board b, Player player1, Player player2)
+		{
+			board = b;
+			p1 = player1;
+			p2 = player2;
+		}
+		
+		public void playGame()
+		{
+			boolean p1Turn;
+			if(p1.getMarker() == 'X')
+			{
+				p1Turn = true;
+			}
+			else
+			{
+				p1Turn = false;
+			}
+			for(int i = 0; i < 9; i++)
+			{
+				board.draw();
+				boolean validTurn;
+				if(p1Turn)
+				{
+					do
+					{
+						validTurn = board.makeTurn(p1.getMarker(), p1.getPlayerMove());
+					}while(!validTurn);
+					if(board.checkForWin(p1.getMarker()))
+					{
+						System.out.println(p1.getMarker() + " is winner!");
+						return;
+					}
+				}
+				else
+				{
+					do
+					{
+						validTurn = board.makeTurn(p2.getMarker(), p2.getPlayerMove());
+					}while(!validTurn);
+					if(board.checkForWin(p1.getMarker()))
+					{
+						System.out.println(p1.getMarker() + " is winner!");
+						return;
+					}
+				}
+				p1Turn = !p1Turn;
+			}
+		}
+		
         public static void main(String[] args)
         {
+			Scanner sc = new Scanner(System.in);
+			char marker;
+			do
+			{
+				System.out.println("Welcome to tic-tac-toe, would you like to play as x or o? ");
+				marker = Character.toUpperCase(sc.findInLine(".").charAt(0));
+			}while(marker != 'x' && marker != 'X' && marker != 'o' && marker != 'O');
+			Player human = new Player(true, marker);
+			Player cpu;
+			if(marker == 'X')
+			{
+				cpu = new Player(false, 'O');
+			}
+			else
+			{
+				cpu = new Player(false, 'X');
+			}
             System.out.println("Let's play");
 			Board b = new Board();
-			b.draw();
+			TicTacToe tictactoe = new TicTacToe(b, human, cpu);
+			tictactoe.playGame();
         }
 }
