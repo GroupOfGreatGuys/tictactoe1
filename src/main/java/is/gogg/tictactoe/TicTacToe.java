08d94aded7dec6c6fd0b8d0c5422f9e893446c92
matthiassig12 <matsig91@gmail.com>
2013-11-23 20:32:09 +0000

    seperated web and console apps

--- src/main/java/is/gogg/tictactoe/TicTacToe.java
+++ src/main/java/is/gogg/tictactoe/TicTacToe.java
@@ -62,8 +62,7 @@ public class TicTacToe
 		
         public static void main(String[] args)
         {
-			staticFileLocation("/public");
-			/*Scanner sc = new Scanner(System.in);
+			Scanner sc = new Scanner(System.in);
 			char marker;
 			do
 			{
@@ -83,73 +82,6 @@ public class TicTacToe
             System.out.println("Let's play");
 			Board b = new Board();
 			TicTacToe tictactoe = new TicTacToe(b, human, cpu);
-			tictactoe.playGame();*/
-			post(new Route("/setMarker") {
-            @Override
-            public Object handle(Request request, Response response) {
-                char marker = Character.toUpperCase(String.valueOf(request.queryParams("marker")).charAt(0));
-				System.out.println(marker);
-                p1 = new Player(true, marker);
-				if(marker == 'X')
-				{
-					p2 = new Player(false, 'O');
-				}
-				else
-				{
-					p2 = new Player(false, 'X');
-				}
-				board = new Board();
-				return marker;
-            }
-        });
-		post(new Route("/getSquare") {
-            @Override
-            public Object handle(Request request, Response response) {
-				String square = String.valueOf(request.queryParams("square"));
-				String valueToReturn = "";
-				int p1Square;
-				int p2Square;
-				boolean valid;
-				if(p1.getMarker() == 'X')
-				{
-					p1Square = p1.getHumanMove(square);
-					valid = board.makeTurn(p1.getMarker(), p1Square);
-					do
-					{
-						p2Square = p2.getPlayerMove();
-						
-						if(board.isTie())
-						{
-							
-							return (String.valueOf(p1Square) + "ht"); 
-						}
-						valid = board.makeTurn(p2.getMarker(), p2Square);
-						
-					}while(!valid);
-				}
-				else
-				{
-					do
-					{
-						p2Square = p2.getPlayerMove();
-						valid = board.makeTurn(p2.getMarker(), p2Square);
-					}while(!valid);
-					p1Square = p1.getHumanMove(square);
-					valid = board.makeTurn(p1.getMarker(), p1Square);
-				}
-				valueToReturn = valueToReturn + String.valueOf(p1Square) + String.valueOf(p2Square);
-				
-				if(board.checkForWin(p1.getMarker()))
-				{
-					valueToReturn =  valueToReturn + 'w';
-				}
-				else if(board.checkForWin(p2.getMarker()))
-				{
-					valueToReturn = valueToReturn + 'l';
-				}
-				
-				return valueToReturn;
-            }
-        });
+			tictactoe.playGame();
         }
 }
