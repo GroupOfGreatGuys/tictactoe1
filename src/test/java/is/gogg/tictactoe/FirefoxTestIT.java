545ed07065831f56cf1b5fc08cd9dae776c5d8e2
matthiassig12 <matsig91@gmail.com>
2013-11-24 19:33:03 +0000

    added staging server variable to firefoxtest

--- src/test/java/is/gogg/tictactoe/FirefoxTestIT.java
+++ src/test/java/is/gogg/tictactoe/FirefoxTestIT.java
@@ -19,7 +19,7 @@ public class FirefoxTestIT {
 	@Before
 	public void setUp() throws Exception {
 		WebDriver driver = new FirefoxDriver();
-		String baseUrl = "http://goggapp.herokuapp.com/";
+		String baseUrl = System.getenv("STAGING_SERVER");
 		selenium = new WebDriverBackedSelenium(driver, baseUrl);
 	}
 
